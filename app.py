04ff5f141b461b30f2a72ca90f8aedd78ce7cbee
manzanareslopezdionicio <manzanareslopezdionicio@gmail.com>
2023-05-28 20:39:13 -0600

    login
    Crear el login de sesión

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/registro')
+def registroc():
+    return render_template('registro.html')
+
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
