7d0b48eee5ef4c7575053b196275b9f4a076cf2c
jonatan <villegasjoncena7@gmail.com>
2023-06-11 20:41:18 -0600

    creacion de funcion registro

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, request
 from flask import render_template
+import database as dbase
+import schema
+
+db = dbase.conection_db()
 
 app = Flask(__name__)
 @app.route('/')
@@ -14,9 +18,35 @@ def login():
 def index():
     return render_template('index.html')
 
-@app.route('/registro')
+@app.route('/registro',methods=["GET", "POST"])
 def registro():
+    if request.method == 'POST':  
+        TablaU = db['usuario']
+     # Obtener las propiedades del formulario
+        email = request.form['email']
+        cedula = request.form['cedula']
+        nombre = request.form['nombre']
+        apellido = request.form['apellido']
+        direccion = request.form['direccion']
+        rol = request.form['rol']
+
+        # validando que los datos existan 
+        if email and cedula and nombre and apellido and direccion and rol :
+         #creando el esquema de usuario
+         usuario = schema.Usuario(email,cedula,nombre,apellido,direccion,rol)
+         #insertando usuario en la tabla 
+         TablaU.insert_one(usuario.__dict__)
+
+         #mensaje de confirmacion  
+         mensaje = "Usuario creado correctamente"
+
+        else:
+          mensaje = "Error al crear el usuario"
+          
+        return redirect(url_for('login'))
+    
     return render_template('registro.html')
+    
 
 @app.route("/orden")
 def orden():
