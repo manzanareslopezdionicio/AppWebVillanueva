a6a5a99a2ce1fb8e52c47310ded368d7f84219bc
jonatan <villegasjoncena7@gmail.com>
2023-06-11 20:50:47 -0600

    esquema de todas las entidades

---
+++ schema.py
@@ -0,0 +1,9 @@
+class Usuario:
+    def __init__(self, email, cedula, nombre, apellido, direccion, rol):
+        self.email = email
+        self.cedula = cedula
+        self.nombre = nombre
+        self.apellido = apellido
+        self.direccion = direccion
+        self.rol = rol
+        
