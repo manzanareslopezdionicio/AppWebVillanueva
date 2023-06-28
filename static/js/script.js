d280f3ab20c4b8eb53af08edca72baf960b782d5
manzanareslopezdionicio <118032880+manzanareslopezdionicio@users.noreply.github.com>
2023-06-28 11:59:59 -0600

    Validacion de formularios

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,7 @@
+//Mensajes de alectas
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
 
 $(document).ready(function () {
     $('#example').DataTable({
@@ -246,3 +248,24 @@ $(document).ready(function () {
           },
     });
 });
+
+//Validacion de input
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
