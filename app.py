e9ec8cd484dd2b141a15b35ea99a8e03092a9377
manzanareslopezdionicio <118032880+manzanareslopezdionicio@users.noreply.github.com>
2023-06-30 17:07:04 -0600

    modificar login

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ from flask import render_template
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return render_template('index.html')
-
-@app.route('/login')
-def login():
     return render_template('login.html')
 
+"""@app.route('/login')
+def login():
+    return render_template('login.html')
+"""
 @app.route('/index')
 def index():
     return render_template('index.html')
