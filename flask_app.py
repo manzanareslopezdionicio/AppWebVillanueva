5e7d568f6df9380da32408811978474b6e0a548d
manzanareslopezdionicio <118032880+manzanareslopezdionicio@users.noreply.github.com>
2024-08-28 23:03:18 -0600

    si
    si

--- flask_app.py
+++ flask_app.py
@@ -4,7 +4,7 @@ from flask import render_template
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('login.html')
 
 """@app.route('/login')
 def login():
